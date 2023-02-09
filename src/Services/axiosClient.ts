2f76bb710f9ccd181aef58b0a4ff5e8b3c7a0cac
DucQuang <lynguyencong2081999@gmail.com>
2023-02-09 11:45:14 +0700

    change baseURL

--- src/Services/axiosClient.ts
+++ src/Services/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import store from "configStore";
 const axiosClient = axios.create({
-  baseURL: "https://elearningnew.cybersoft.edu.vn/api/",
+  baseURL: "https://elearning0706.cybersoft.edu.vn/api/",
   headers: {
     tokenCybersoft:
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJGcm9udCBFbmQgNzEiLCJIZXRIYW5TdHJpbmciOiIyOS8xMi8yMDIyIiwiSGV0SGFuVGltZSI6IjE2NzIyNzIwMDAwMDAiLCJuYmYiOjE2NDU5ODEyMDAsImV4cCI6MTY3MjQxOTYwMH0.SZe3CJl1OkNH-0zfzqOV0CSC8WZ6q2hw64UykpCytT0",
